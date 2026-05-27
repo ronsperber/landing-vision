360800a8ee4e885a8ecb4dd40a4fe388e5360099
ronsperber <85753940+ronsperber@users.noreply.github.com>
2026-05-27 16:46:36 -0400

    updated regression to multiple dropout parameters

--- scripts/train.py
+++ scripts/train.py
@@ -16,7 +16,9 @@ from network.training import train
 
 parser = ArgumentParser()
 parser.add_argument("-e", "--epochs", type=int, default=500)
-parser.add_argument("-d", "--dropout", type=float, default=0.0)
+parser.add_argument("--cnn_dropout", type=float, default=0.1)
+parser.add_argument("--linear_dropout", type=float, default=0.3)
+parser.add_argument("--lstm_dropout", type=float, default=0.4)
 args = parser.parse_args()
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 DATA_PATH = "data/preprocessed.h5"
@@ -40,7 +42,11 @@ joblib.dump(scaler, OUTPUT_PATH / "reward_scaler.pkl")
 print("Scaler fit and saved.")
 train_loader = DataLoader(train_ds, batch_size=2, shuffle=True)
 val_loader = DataLoader(val_ds, batch_size=2, shuffle=False)
-model = LunarLanderConv(dropout_rate=args.dropout)
+model = LunarLanderConv(
+    cnn_dropout=args.cnn_dropout,
+    linear_dropout=args.linear_dropout,
+    lstm_dropout=args.lstm_dropout,
+)
 hist, val_hist = train(
     model=model,
     train_loader=train_loader,
